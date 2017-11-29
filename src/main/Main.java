d93ffb95c62d4a80e35577b7d3afa6c709099490
Mohamed Sherif <moma652@gmail.com>
2017-11-29 16:52:28 +0200

    initial packet sniff

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,45 @@
 package main;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.pcap4j.core.*;
+import org.pcap4j.core.PcapNetworkInterface.*;
+import org.pcap4j.packet.IpV4Packet;
+import org.pcap4j.packet.Packet;
+import org.pcap4j.util.NifSelector;
+
 public class Main {
   public static void main(String argv[]){
-    System.out.println("Texteditor Test");
+    InetAddress addr;
+    PcapNetworkInterface nif;
+    try{
+        addr = InetAddress.getByName("192.168.1.1");
+        nif = new NifSelector().selectNetworkInterface();
+        if (nif == null) System.exit(1);
+        final PcapHandle handle = nif.openLive(65536, PromiscuousMode.PROMISCUOUS, 10);
+
+        PacketListener listener = new PacketListener() {
+            @Override
+            public void gotPacket(Packet packet) {
+                printPacket(packet, handle);
+            }
+        };
+        handle.loop(5, listener);
+    }catch (UnknownHostException e){
+        System.out.println("here" + e.toString());
+    }catch (Exception e){
+        System.out.println(e.toString());
+    }
+  }
+
+  private static void printPacket(Packet packet, PcapHandle ph){
+      StringBuilder sb = new StringBuilder();
+      sb.append("A Packet Captured at ")
+              .append(ph.getTimestamp())
+              .append(":");
+      System.out.println(sb);
+      System.out.println(packet);
   }
 }
